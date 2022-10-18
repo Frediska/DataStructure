12536a69eec517c9a084299d97d22e766a3c5864
Александр Макаров <artemi91obo@rambler.ru>
2022-10-18 18:11:54 +0700

    lab 1 - correct

--- LAB 1/LAB 1/Main.cpp
+++ LAB 1/LAB 1/Main.cpp
@@ -78,6 +78,7 @@ int LinearSearch(DynamicArray* dynamicArray, int element)
         if (dynamicArray->array[i] = element)
         {
             return i;
+            cout << i;
         }
     }
     return -1;
@@ -147,63 +148,91 @@ int main()
 {
     DynamicArray* array = new DynamicArray;
 
-    int taskNumber;
-    cout << "Enter number of action: ";
-    cin >> taskNumber;
-    cout << "1.Create dynamic array. \n2. Initialize an dynamic array with random numbers."
+    cout << "1. Create dynamic array. \n2. Initialize an dynamic array with random numbers. \n"
         "3. Add new element in dynamic array.\n4. Remove element of dynamic array.\n"
         "5. Insert element at index into dynamc array.\n6. Sort dynamic array.\n"
-        "7. Linear Search.\n8.Binary search.\n"
+        "7. Linear Search.\n8. Binary search.\n"
         "9. Show dynamic array.\n" << endl;
-    cout << endl;
-
-    switch (taskNumber)
+    
+    while (true)
     {
-    case 1:
-        CreateDynamicArray(array);
-        break;
-    case 2:
-        GetRandomArray(array, 4);
-        break;
-    case 3:
-        int element;
-        cout << "Enter which element you want to add: ";
-        cin >> element;
-        AddNewElement(array, element);
-        break;
-    case 4:
-        int index;
-        cout << "Enter index which you want to remove: ";
-        cin >> index;
-        RemoveElement(array, index);
-        break;
-    case 5:
-        index;
-        element;
-        cout << "Enter the index of the element where you want to insert the element: ";
-        cin >> index;
-        cout << "Enter which element you want to add: ";
-        cin >> element;
-        InsertElement(array, element, index);
-        break;
-    case 6:
-        SortArray(array);
-        break;
-    case 7:
-        element;
-        cout << "Entee element which you want to find: ";
-        cin >> element;
-        LinearSearch(array, element);
-        break;
-    case 8:
-        element;
-        cout << "Entee element which you want to find: ";
-        cin >> element;
-        BinarySearch(array, element);
-        break;
-    case 9:
-        ShowDynamicArray(array);
-        break;
+        int taskNumber;
+        cout << "Enter number of action: ";
+        cin >> taskNumber;
+        cout << endl;
+
+        switch (taskNumber)
+        {
+        case 1:
+        {
+            CreateDynamicArray(array);
+            break;
+        }  
+        case 2:
+        {
+            GetRandomArray(array, 4);
+            break;
+        } 
+        case 3:
+        {
+            int element;
+            cout << "Enter which element you want to add: ";
+            cin >> element;
+            AddNewElement(array, element);
+            break;
+        }    
+        case 4:
+        {
+            int index;
+            cout << "Enter index which you want to remove: ";
+            cin >> index;
+            RemoveElement(array, index);
+            break;
+        }   
+        case 5:
+        {
+            int index;
+            int element;
+            cout << "Enter the index of the element where you want to insert the element: ";
+            cin >> index;
+            cout << "Enter which element you want to add: ";
+            cin >> element;
+            InsertElement(array, element, index);
+            break;
+        }    
+        case 6:
+        {
+            SortArray(array);
+            break;
+        }    
+        case 7:
+        {
+            int element;
+            cout << "Enter element which you want to find: ";
+            cin >> element;
+            LinearSearch(array, element);
+            break;
+        }    
+        case 8:
+        {
+            int element;
+            cout << "Enter element which you want to find: ";
+            cin >> element;
+            BinarySearch(array, element);
+            break;
+        }  
+        case 9:
+        {
+            ShowDynamicArray(array);
+            break;
+        }
+        default:
+        {
+            cout << "Enter correct value!";
+            break;
+        }
+        }
     }
+    
 }
 
